a81c86c94bf2295966a324e46613b9670e929ae5
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2020-10-04 23:06:11 +0200

    Fix deletion of old files

--- src/scripts/fetchAushang.sh
+++ src/scripts/fetchAushang.sh
@@ -8,7 +8,7 @@ find /data/from_mail/ -type f -iname "*.pdf" -exec mv {} /data/live/ \;
 rm -rf /data/from_mail/*
 
 touch /data/timeref -d "1 month ago"
-find /data/live/ -not -cnewer /data/timeref -delete
+find /data/live/ -not -newer /data/timeref -delete
 
 chown -R www-data:www-data /data/live/
 chmod -R u=rwX,g=rX,o=rX /data/live/
