05106cd2d064900b089b975a23eb723cc4759f12
Philipp <philipp.grunenberg@t-online.de>
2026-05-19 19:32:02 +0200

    Add .ds_store to .gitignore and update base path handling in index.php

--- src/website/index.php
+++ src/website/index.php
@@ -5,6 +5,15 @@
     <title>FIUS Aushang</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    $basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+    if ($basePath === '' || $basePath === '.') {
+        $basePath = '/';
+    } else {
+        $basePath .= '/';
+    }
+    ?>
+    <base href="<?php echo htmlspecialchars($basePath, ENT_QUOTES, 'UTF-8'); ?>">
     <link rel="stylesheet" href="stylesheet.css" />
 </head>
 
